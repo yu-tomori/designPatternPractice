773614075d9b7684bcf7928e2b9be1adfd053d69
yu <peacetomori1118@gmail.com>
2020-04-24 17:24:39 +0900

    第二章Adapter完遂!

---
+++ Adapter/PrintBanner.java
@@ -0,0 +1,12 @@
+
+public class PrintBanner extends Banner implements Print {
+    public PrintBanner(String string) {
+        super(string);
+    }
+    public void printWeak() {
+        showWithParen();
+    }
+    public void printStrong() {
+        showWithAster();
+    }
+}
