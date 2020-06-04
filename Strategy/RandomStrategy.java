ad5f769325f70c20203de40750cf2f7c018c78ec
yu <peacetomori1118@gmail.com>
2020-06-04 09:54:46 +0900

    第10章Strategy練習問題10-1done!

--- Strategy/RandomStrategy.java
+++ Strategy/RandomStrategy.java
@@ -1,3 +1,14 @@
-public class RandomStrategy {
-    
+import java.util.Random;
+
+public class RandomStrategy implements Strategy {
+    private Random random;
+    public RandomStrategy(int seed) {
+        random = new Random(seed);
+    }
+    public Hand nextHand() {
+        return Hand.getHand(random.nextInt(3));
+    }
+    public void study(boolean win) {
+        
+    }
 }
