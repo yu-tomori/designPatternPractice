b879226daad7670afdf67272df182621fe8c1e51
yu <peacetomori1118@gmail.com>
2020-07-17 02:14:49 +0900

    第13章Visitor done!

---
+++ Visitor/ListVisitor.java
@@ -0,0 +1,19 @@
+import java.util.Iterator;
+
+public class ListVisitor extends Visitor {
+    private String currentdir = "";
+    public void visit(File file) {
+        System.out.println(currentdir + "/" + file);
+    }
+    public void visit(Directory directory) {
+        System.out.println(currentdir + "/" + directory);
+        String savedir = currentdir; 
+        currentdir = currentdir + "/" + directory.getName();
+        Iterator it = directory.iterator();
+        while (it.hasNext()) {
+            Entry entry = (Entry)it.next();
+            entry.accept(this);
+        }
+        currentdir = savedir;
+    }
+}
