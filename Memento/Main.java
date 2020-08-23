ad897283dc82d1916f8f3befda543f0398cde93c
yu <peacetomori1118@gmail.com>
2020-08-23 15:24:48 +0900

    第19章Memento練習問題done!

--- Memento/Main.java
+++ Memento/Main.java
@@ -1,10 +1,22 @@
 import game.Memento;
 import game.Gamer;
+import java.io.*;
 
 public class Main {
+    public static final String SAVEFILENAME = "game.dat";
     public static void main(String[] args) {
+        File file = new File("game.dat");
+
         Gamer gamer = new Gamer(100);
-        Memento memento = gamer.createMemento();
+        Memento memento = null;
+        try {
+            ObjectInputStream ois = new ObjectInputStream(new FileInputStream(SAVEFILENAME));
+            memento = (Memento)ois.readObject();
+            gamer.restoreMemento(memento);
+            ois.close();
+        } catch (Exception e) {
+        }
+        memento = gamer.createMemento();
         for (int i = 0; i < 100; i++) {
             System.out.println("==== " + i);
             System.out.println("現状:" + gamer);
@@ -15,10 +27,23 @@ public class Main {
 
             if (gamer.getMoney() > memento.getMoney()) {
                 System.out.println("    (だいぶ増えたので、現在の状態を保存しておこう) ");
-                memento = gamer.createMemento();
+                try {
+                    memento = (Memento)gamer.createMemento();
+                    ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(SAVEFILENAME));
+                    oos.writeObject(memento);
+                    oos.close();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
             } else if (gamer.getMoney() < memento.getMoney() / 2) {
                 System.out.println("    (だいぶ減ったので、以前の状態に復帰しよう");
-                gamer.restoreMemento(memento);
+                try {
+                    ObjectInputStream ois = new ObjectInputStream(new FileInputStream(SAVEFILENAME));
+                    memento = (Memento)ois.readObject();
+                    gamer.restoreMemento(memento);
+                    ois.close();
+                } catch (Exception e) {
+                }
             }
 
             try {
--- Memento/game/Memento.java
+++ Memento/game/Memento.java
@@ -1,7 +1,8 @@
 package game;
 import java.util.*;
+import java.io.Serializable;
 
-public class Memento {
+public class Memento implements Serializable {
     int money;
     ArrayList fruits;
     public int getMoney() {
@@ -12,7 +13,7 @@ public class Memento {
         this.fruits = new ArrayList();
     }
     void addFruit(String fruit) {
-        fruits.add(fruit);
+        this.fruits.add(fruit);
     }
     List getFruits() {
         return (List)fruits.clone();
